27872cbee3827325af574211472cfbcbcffff3b0
roshanshresthanp <stha901@gmail.com>
2025-01-08 16:52:44 +0000

    Added weather icon and changes made on UI

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [city,setCity] = useState("Nepal");
+  const [city, setCity] = useState("London");
   const [weatherData, setWeatherData] = useState(null);
 
   const getWeatherIcon = (main) => {
     switch (main) {
       case 'Clouds':
-      return "./icons/cloudy.png";
-      break;
+        return "./icons/cloudy.png";
+        break;
 
       case 'Clear':
-      return "./icons/cloudy.png";
-      break;
+        return "./icons/cloudy.png";
+        break;
 
-      case 'Rain':
-      return "./icons/rain.png";
-      break;
-
-      case 'Mist':
-      return "./icons/clouds.png";
-      break;
-
-      case 'Haze':
-      return "./icons/sun.png";
-      break;
-      
       case 'Rain':
         return "./icons/rain.png";
+        break;
+
+      case 'Mist':
+        return "./icons/haze.png";
+        break;
+
+      case 'Haze':
+        return "./icons/haze.png";
+        break;
+
+      case 'Wind':
+        return "./icons/wind.png";
         break;
       default:
         return null;
@@ -49,7 +48,7 @@ function App() {
   const API_KEY = "24deb6a0ac1f60637fcb29cb564a555a";
 
   const fetchWeatherData = async () => {
-    try{
+    try {
 
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
@@ -57,18 +56,23 @@ function App() {
 
       const data = await response.json();
       console.log(data);
-
       setWeatherData(data)
+      if (data.cod !== 200) { // API returns cod: 200 on success
+        alert("Please enter a valid city name");
+        // throw new Error(data.message); // Throw error with message from API
+        window.location.reload();
+      }
 
-    }catch(error){
+    } catch (error) {
       console.log(error)
+      setWeatherData(null)
     }
-    
+
   }
 
-  useEffect(()=>{
-      fetchWeatherData();
-  },[])
+  useEffect(() => {
+    fetchWeatherData();
+  }, [])
 
   const handleInputChange = (event) => {
     console.log(event.target.value);
@@ -76,56 +80,43 @@ function App() {
   }
 
   const handleSubmit = (event) => {
-     event.preventDeffault();
-     fetchWeatherData();
+    event.preventDefault();
+    fetchWeatherData();
   }
 
-  const intoC = (f) =>{
-    return (f-273.15).toFixed(0);
+  const intoC = (f) => {
+    return (f - 273.15).toFixed(0);
   }
 
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <div className="weather-app">
+      <h3>Weather App </h3>
+      <div className="weather-app" >
 
         {weatherData && 
-        <>
+          <>
+            <div className="location">{weatherData.name}</div>
+            <div className="date">{date}</div>
+            <div className="icon">
+              <img height="50px" width="50px" src={getWeatherIcon(weatherData.weather[0].main)} />
+            </div>
+            <div className="temperature">{intoC(weatherData.main.temp)} °C</div>
+            <div className="description">{weatherData.weather[0].main}</div>
+            <div className="details">
 
-        <div className="location">{weatherData.name}</div>
-                  <div className="date">{date}</div>
-                  <div className="icon">
-                    <img height="50px" width="50px" src={getWeatherIcon(weatherData.weather[0].main)} />
-                    
+              <form className='form' onSubmit={handleSubmit}>
+                <input type="text" className='form-control' placeholder='Enter City' name="city" onChange={handleInputChange}></input>
+                <button type="submit" className='button'>Submit</button>
+              </form>
+            </div>
 
-                  </div>
-                  <div className="temperature">{intoC(weatherData.main.temp)} °C</div>
-                  <div className="description">{weatherData.weather[0].main}</div>
-                  <div className="details">
+          </>}
 
-                    <form className='form' onSubmit={handleSubmit}>
-                      <input type="text" placeholder='Enter City' name="city" onChange={handleInputChange}></input>
-                      <button type="submit">Submit</button>
-                  </form>
-                    {/* <div>
+      </div>
 
-                      <span>Humidity</span>
-                      <p>45%</p>
-                    </div>
-                    <div>
-                      <span>Wind</span>
-                      <p>10 km/h</p>
-                    </div> */}
-                  </div>
-        
-        </>}
-         
-          </div>
-          
 
     </div>
   );
 }
 
 export default App;
- 
